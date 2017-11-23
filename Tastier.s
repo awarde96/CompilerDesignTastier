33df5ad61ba39d23fd1a320c068162b819a69213
Adam <awarde@tcd.ie>
2017-11-23 03:37:54 -0800

    Made fall through work

--- Tastier.s
+++ Tastier.s
@@ -3,11 +3,17 @@ MainBody
     LDR R2, =2
     ADD R2, R4, R2, LSL #2
     STR R5, [R2] ; xy
+    ADD     R0, PC, #4      ; string address
+    BL      TastierPrintString
+    B       L1
+    DCB     "Constant xy = ", 0
+    ALIGN
+L1
     LDR R2, =2
     ADD R2, R4, R2, LSL #2
     LDR R5, [R2] ; xy
     MOV     R0, R5
-    BL      TastierPrintInt
+    BL      TastierPrintIntLf
     LDR     R5, =8
     LDR R2, =3
     ADD R2, R4, R2, LSL #2
@@ -18,11 +24,17 @@ MainBody
     LDR R2, =0
     ADD R2, R4, R2, LSL #2
     STR R0, [R2] ; i
+    ADD     R0, PC, #4      ; string address
+    BL      TastierPrintString
+    B       L2
+    DCB     "[0]zzz = ", 0
+    ALIGN
+L2
     LDR R2, =0
     ADD R2, R4, R2, LSL #2
     LDR R5, [R2] ; i
     MOV     R0, R5
-    BL      TastierPrintInt
+    BL      TastierPrintIntLf
     LDR     R5, =9
     LDR R2, =4
     ADD R2, R4, R2, LSL #2
@@ -45,17 +57,18 @@ MainBody
     LDR R2, =0
     ADD R2, R4, R2, LSL #2
     STR R0, [R2] ; i
-    LDR R2, =0
-    ADD R2, R4, R2, LSL #2
-    LDR R5, [R2] ; i
-    MOV     R0, R5
-    BL      TastierPrintInt
+    ADD     R0, PC, #4      ; string address
+    BL      TastierPrintString
+    B       L3
+    DCB     "[2][2]yyy = ", 0
+    ALIGN
+L3
     LDR R2, =0
     ADD R2, R4, R2, LSL #2
     LDR R5, [R2] ; i
     MOV     R0, R5
     BL      TastierPrintIntLf
-    LDR     R5, =4
+    LDR     R5, =3
     LDR R2, =0
     ADD R2, R4, R2, LSL #2
     STR R5, [R2] ; i
@@ -67,57 +80,58 @@ MainBody
     MOVEQ   R6, #1
     MOVNE   R6, #0
     MOVS    R6, R6          ; reset Z flag in CPSR
-    BEQ     L2              ; jump on condition false
+    BEQ     L5              ; jump on condition false
     LDR     R5, =1
     LDR R2, =1
     ADD R2, R4, R2, LSL #2
     STR R5, [R2] ; j
-L2
+L5
     LDR     R6, =2
     CMP     R6, R5
     MOVEQ   R6, #1
     MOVNE   R6, #0
     MOVS    R6, R6          ; reset Z flag in CPSR
-    BEQ     L3              ; jump on condition false
+    BEQ     L6              ; jump on condition false
     LDR     R5, =2
     LDR R2, =1
     ADD R2, R4, R2, LSL #2
     STR R5, [R2] ; j
-    B       L1
-L3
+    B       L4
+L6
     LDR     R6, =3
     CMP     R6, R5
     MOVEQ   R6, #1
     MOVNE   R6, #0
     MOVS    R6, R6          ; reset Z flag in CPSR
-    BEQ     L4              ; jump on condition false
+    BEQ     L7              ; jump on condition false
     LDR     R5, =3
     LDR R2, =1
     ADD R2, R4, R2, LSL #2
     STR R5, [R2] ; j
-L4
+L7
     LDR     R6, =4
-    CMP     R6, R5
-    MOVEQ   R6, #1
-    MOVNE   R6, #0
-    MOVS    R6, R6          ; reset Z flag in CPSR
-    BEQ     L5              ; jump on condition false
     LDR     R5, =4
     LDR R2, =1
     ADD R2, R4, R2, LSL #2
     STR R5, [R2] ; j
-    B       L1
-L5
+    B       L4
+L8
     LDR     R5, =10
     LDR R2, =1
     ADD R2, R4, R2, LSL #2
     STR R5, [R2] ; j
-L1
+L4
+    ADD     R0, PC, #4      ; string address
+    BL      TastierPrintString
+    B       L9
+    DCB     "j = ", 0
+    ALIGN
+L9
     LDR R2, =1
     ADD R2, R4, R2, LSL #2
     LDR R5, [R2] ; j
     MOV     R0, R5
-    BL      TastierPrintInt
+    BL      TastierPrintIntLf
 StopTest
     B       StopTest
 Main
--- generated/Scanner.cs
+++ generated/Scanner.cs
@@ -229,8 +229,8 @@ public class UTF8Buffer: Buffer {
 public class Scanner {
 	const char EOL = '\n';
 	const int eofSym = 0; /* pdt */
-	const int maxT = 45;
-	const int noSym = 45;
+	const int maxT = 46;
+	const int noSym = 46;
 
 
 	public Buffer buffer; // scanner buffer
@@ -393,19 +393,20 @@ public class Scanner {
 			case "void": t.kind = 15; break;
 			case "switch": t.kind = 28; break;
 			case "case": t.kind = 30; break;
-			case "break": t.kind = 31; break;
-			case "default": t.kind = 32; break;
-			case "if": t.kind = 33; break;
-			case "else": t.kind = 34; break;
-			case "while": t.kind = 35; break;
-			case "read": t.kind = 36; break;
-			case "write": t.kind = 37; break;
-			case "writeln": t.kind = 38; break;
-			case "program": t.kind = 39; break;
-			case "int": t.kind = 40; break;
-			case "bool": t.kind = 41; break;
-			case "const": t.kind = 43; break;
-			case "array": t.kind = 44; break;
+			case "continue": t.kind = 31; break;
+			case "break": t.kind = 32; break;
+			case "default": t.kind = 33; break;
+			case "if": t.kind = 34; break;
+			case "else": t.kind = 35; break;
+			case "while": t.kind = 36; break;
+			case "read": t.kind = 37; break;
+			case "write": t.kind = 38; break;
+			case "writeln": t.kind = 39; break;
+			case "program": t.kind = 40; break;
+			case "int": t.kind = 41; break;
+			case "bool": t.kind = 42; break;
+			case "const": t.kind = 44; break;
+			case "array": t.kind = 45; break;
 			default: break;
 		}
 	}
@@ -485,7 +486,7 @@ public class Scanner {
 			case 21:
 				{t.kind = 27; break;}
 			case 22:
-				{t.kind = 42; break;}
+				{t.kind = 43; break;}
 			case 23:
 				recEnd = pos; recKind = 19;
 				if (ch == '=') {AddCh(); goto case 16;}
